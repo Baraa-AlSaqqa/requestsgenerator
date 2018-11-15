5aeb35c8828f9e7e42bf494f0c12875ec6e80ac0
Baraa Al-Saqqa <baraa.sk@gmail.com>
2018-11-15 09:28:35 +0200

    Init Project

---
+++ index.js
@@ -0,0 +1,45 @@
+const METHODS = ["CHECKOUT", "CONNECT", "COPY", "DELETE", "GET", "HEAD", "LOCK", "M-SEARCH", "MERGE", "MKACTIVITY", "MKCOL", "MOVE", "NOTIFY", "OPTIONS", "PATCH", "POST", "PROPFIND", "PROPPATCH", "PURGE", "PUT", "REPORT", "SEARCH", "SUBSCRIBE", "TRACE", "UNLOCK", "UNSUBSCRIBE"];
+var URLpath = '/';
+var attribute = '';
+export default class API {
+
+    static setUrl(url, attr) {
+        URLpath = url;
+        attribute = attr;
+    }
+
+    static http = new Proxy({}, {
+        get(target, propKey) {
+            const method = METHODS.find(method =>
+                propKey.startsWith(method.toLowerCase()))
+            if (!method) return;
+            const path =
+                URLpath +
+                propKey
+                .substring(method.length)
+                .replace(/([a-z])([A-Z])/g, '$1/$2')
+                .replace(/\$/g, '/$/')
+                .toLowerCase()
+            return (...args) => {
+                const finalPath = path.replace(/\$/g, () => args.shift())
+                const queryOrBody = args.shift() || {}
+
+                if (method.toLowerCase() == 'get' || method.toLowerCase() == "head") {
+                    let query = Object.keys(queryOrBody)
+                        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(queryOrBody[k]))
+                        .join('&');
+                    var url = finalPath + '?' + query;
+                    return fetch(url + (attribute || ''), {
+                        method: method
+                    });
+                }
+                return fetch(finalPath + (attribute || ''), {
+                    method: method,
+                    body: queryOrBody.body != {} ? queryOrBody.body : {},
+                    header: queryOrBody.header != {} ? queryOrBody.header : {},
+                    mode: 'cors'
+                });
+            }
+        }
+    });
+}
